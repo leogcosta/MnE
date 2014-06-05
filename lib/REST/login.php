d335df6b2c88a4f68f4e3edffc2ec64ba8649d00
moe <moe.duffdude@gmail.com>
2014-06-05 15:46:07 +0300

    logout n' stuff...

--- lib/REST/login.php
+++ lib/REST/login.php
@@ -24,5 +24,9 @@
    * @return HTTP JSON Response {success: (true | false)}
    */
   function login_DELETE () {
+    user_login('', '', TRUE);
+    // should i go with 403: Forbidden or 410 Unauthorized
+    // i choose 403 - gives it a nice bad ass look!
+    logout() ? Flight::json(['message' => 'logged out, now that you are'], 202) : Flight::json(['message' => 'YOU SHALL NOT LOGOUT!'], 403);
   }
 ?>

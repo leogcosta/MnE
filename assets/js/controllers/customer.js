753d543c68ed0417b4210422e99a95309594f5bb
moe <moe.duffdude@gmail.com>
2014-06-06 16:06:05 +0300

    i don't need it
    - naming `standard` is the plural form now

--- assets/js/controllers/customer.js
+++
@@ -1,4 +0,0 @@
-var customersCtrl = app.controller('customersCtrl', ['$scope', '$location', '$http', 'dbEngine', function ($scope, $location, $http, dbEngine) {
-  dbEngine.bootWebSQL();
-  $scope.customersCtrl = this;
-}]);

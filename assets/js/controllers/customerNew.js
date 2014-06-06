bcc367d3d0db00a4ca58ab383ed786b571ce94fc
moe <moe.duffdude@gmail.com>
2014-06-06 21:52:28 +0300

    test - test
    - Huston we have a launch...puuuuuuuuuuuuuuuuuuuuuuuchhhh (that's
    suppose to the the sound the rocket makes when it takes off - i read
    it's LOUD as shit, like black people in the movies - am black so i can
    say it)

--- assets/js/controllers/customerNew.js
+++ assets/js/controllers/customerNew.js
@@ -7,9 +7,36 @@ var customerNewCtrl = app.controller('customerNewCtrl', ['$rootScope', '$scope',
 
   this.save = function () {
     dbEngine.save('customers', $scope.newCustomer, function (data, status, headers, config) {
+      // someday we will leave in a world where we don't need $rootScope.$apply();
+      // #believe #comingSoon #angularjsIsAwesome
       $location.path('/customers');
+      $rootScope.$apply();
     });
   };
+
+  /*
+  dbEngine.delete('customers', 'customer_id', 14, function (data, status, headers, config) {
+    console.log(data);
+  });
+  */
+
+  /*
+  dbEngine.update('customers', {customer_id: 1, customer_full_name: 'We Come ONE!', customer_phone_number: '0912441676', customer_email: 'moe.duffdude@gmail.com'}, function (data, status, headers, config) {
+    console.log(data);
+  });
+  */
+
+  /*
+  dbEngine.query('customers', function (data, status, headers, config) {
+    console.log(data);
+  });
+  */
+
+  /*
+  dbEngine.get('customers', 99, function (data, status, headers, config) {
+    console.log(data);
+  });
+  */
 
   $scope.customerNewCtrl = this;
 }]);

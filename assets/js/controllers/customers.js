d4037ceabd70f6c05dc40575c815636e5089f20b
moe <moe.duffdude@gmail.com>
2014-06-08 16:26:02 +0300

    sync ready

--- assets/js/controllers/customers.js
+++ assets/js/controllers/customers.js
@@ -9,5 +9,18 @@ var customersCtrl = app.controller('customersCtrl', ['$rootScope', '$scope', '$l
     }
   });
 
+  // whenever 'sync-completed' is fired we're going to `reload` the current
+  // listing, we're not going to be loading ERY where, just where it matters.
+  // `more` code on our side, but worth it --- i think
+  $scope.$on('sync-completed', function () {
+    dbEngine.query('customers', function (data, status, headers, config) {;
+      $scope.customers = data;
+
+      if ($scope.$$phase === null) {
+        $scope.$apply();
+      }
+    });
+  });
+
   $scope.customersCtrl = this;
 }]);

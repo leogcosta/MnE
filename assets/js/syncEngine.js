8146462cc0c2c4421bebb463706b264d20f9e1a9
moe <moe.duffdude@gmail.com>
2014-07-09 09:44:03 +0300

    syncEngine2 looks good
    - only proof reading remains!

--- assets/js/syncEngine.js
+++ assets/js/syncEngine.js
@@ -145,3 +145,165 @@ syncEngine.factory('syncEngine', ['$rootScope', '$q', '$http', 'dbEngine', funct
     return deferred.promise;
   };
 }]);
+
+
+
+// syncEngine ALWAYS requires connectivity with the server - hence, sync
+syncEngine.factory('syncEngine2', ['$rootScope', '$q', '$http', 'dbEngine2', function ($rootScope, $q, $http, dbEngine2) {
+  var that = {};
+
+
+
+  // this will go through the returned merge conflict messages
+  // and it'll change their operation to 'MERGE'
+  var merge = function (tableName, dataMerge, callback) {
+    console.log('DON redecorating ['+ tableName +']!');
+    console.log('MERGING ['+ tableName +']...');
+    var iMergeCount = dataMerge.length, iCount = 0;
+
+    if (iMergeCount > 0) {
+      dbEngine2.webdb.db.transaction(function (SQLTransaction) {
+        for (index in dataMerge) {
+          var sql = {
+            set: [],
+            value: []
+          };
+
+          for (key in dataMerge[index]) {
+            sql.set.push(key +' = ?');
+            if (key === 'operation') {
+              sql.value.push('MERGE');
+            } else {
+              sql.value.push(dataMerge[index][key] === null ? '' : dataMerge[index][key]);
+            }
+          }
+
+          sql.set = sql.set.join(', ');
+          sql.set += ' WHERE '+ dbEngine2.webdb.keys[tableName].primaryKey +' = ?';
+          sql.value.push(dataMerge[index][dbEngine2.webdb.keys[tableName].primaryKey]);
+
+          SQLTransaction.executeSql('UPDATE '+ tableName +' SET '+ sql.set, sql.value, function (SQLTransaction, SQLResultSet) {
+            if (++iCount === iMergeCount) {
+              callback();
+            }
+          }, function (SQLTransaction, error) {
+            if (++iCount === iMergeCount) {
+              callback();
+            }
+          });
+        }
+      }, SQLErrorHandeler);
+    } else {
+      callback();
+    }
+  };
+
+
+
+  // a `light` version of the other stuff
+  // a lot of things should slide without causing a racket...
+  // did i spell that right or...
+  var SQLErrorHandeler = function (SQLTransaction, error) {
+    console.error(error);
+  };
+
+
+
+  // this will delete the local data (except for the exclusion list)
+  // and stuff...
+  // i haven't eaten lunch :(
+  var purgeTable = function (tableName, data, callback) {
+    // this will hold ID that will be omitted when `purging` the table
+    var exclusionList = [];
+    for (index in data.MERGE) {
+      exclusionList.push(data.MERGE[index][dbEngine2.webdb.keys[tableName].primaryKey]);
+    }
+
+    dbEngine2.webdb.db.transaction(function (SQLTransaction) {
+      SQLTransaction.executeSql('DELETE FROM '+ tableName +' WHERE '+ dbEngine2.webdb.keys[tableName].primaryKey +' NOT IN (?)', [exclusionList.join(', ')], function (SQLTransaction, SQLResultSet) {
+        console.log('finished cleaning up ['+ tableName +']!');
+        console.log('redecorating ['+ tableName +']...');
+        var sqlExecutionCount = 0, dataListLength = data.LIST.length;
+
+        for (index in data.LIST) {
+          delete data.LIST[index].operation;
+
+          var sql = {
+            wild: [],
+            value: [],
+            key: Object.keys(data.LIST[index])
+          };
+
+          for (key in sql.key) {
+            sql.wild.push('?');
+            sql.value.push(data.LIST[index][sql.key[key]] === null ? '' : data.LIST[index][sql.key[key]]);
+          }
+
+          sql.wild = sql.wild.join(', ');
+
+          // we get 'around' the sync behavior in a neat way
+          // once we know we're "done" with the redecoration, we start
+          // with the merging process
+          SQLTransaction.executeSql('INSERT INTO '+ tableName +' ('+ sql.key.join(', ') +') VALUES ('+ sql.wild +')', sql.value, function (SQLTransaction, SQLResultSet) {
+            if (++sqlExecutionCount === dataListLength) {
+              // since we are done with the redecoration, we move on to
+              // the merging process --- that way we can charge double
+              // #BOOM!
+              merge(tableName, data.MERGE, function () {
+                console.log('MERGE completed on ['+ tableName +']!');
+              });
+            }
+          }, function (SQLTransaction, error) {
+            if (++sqlExecutionCount === dataListLength) {
+              merge(tableName, data.MERGE, function () {
+                console.log('MERGE completed on ['+ tableName +']!');
+              });
+            }
+          });
+        }
+      }, SQLErrorHandeler);
+    }, SQLErrorHandeler);
+  };
+
+
+
+  that.syncTable = function (tableName, callback) {
+    if ($rootScope.online === true) {
+      // between calling the dbEngine and syncEngine if someone else
+      // called it --- then we would be a heap of trouble :)
+      $rootScope.syncMode = true;
+
+      var syncList = {
+        DELETE: [],
+        SAVE: [],
+        UPDATE: []
+      };
+
+      // we'll fetch the local data with operation tags of
+      // SAVE, DELETE and MERGE and send it to the server and merge whatever is
+      // returned from the server i.e. the server knows best principle
+      dbEngine2.query(tableName, function (data, status, headers, config) {
+        for (row in data) {
+          if (syncList.hasOwnProperty(data[row].operation)) {
+            syncList[data[row].operation].push(data[row]);
+          }
+        }
+
+        $http.post('api/sync/'+ tableName, syncList).success(function (data, status, headers, config) {
+          // merging returned data...
+          // i get a feeling that syncing might become expensive
+          // 100ms of execution time in 4S is my threshold
+          purgeTable(tableName, data, function () {
+            console.log('purge completed!');
+          });
+        }).error(function (data, status, headers, config) {
+          console.error(data);
+        });
+      });
+    }
+  };
+
+
+
+  return that;
+}]);
--- scripts/sync.php
+++ scripts/sync.php
@@ -189,4 +189,152 @@
     Flight::json($response, 202);
     */
   }
+
+
+
+
+
+  function syncTable ($tableName) {
+    $request = json_decode(Flight::request() -> body, TRUE);
+    $tableName = strtoupper($tableName);
+    $mergeList = [];
+
+    foreach ($request as $key => $value) {
+      switch ($key) {
+
+        // on DELETE, there's no problem here
+        // even if the user wanted to delete a un-synced data
+        // he/she will be allowed --- since he/she as 'full' access
+        // to his/her data
+        case 'DELETE':
+          if (isset($GLOBALS['TABLES'][$tableName]['timestamp'])) {
+            foreach ($value as $key1 => $value1) {
+              $backup = $value1;
+
+              $row = getDB() -> get($GLOBALS['TABLES'][$tableName]['tableName'],
+                                    $GLOBALS['TABLES'][$tableName]['tableSelect'],
+                                    [$GLOBALS['TABLES'][$tableName]['tableId'] => $value1[$GLOBALS['TABLES'][$tableName]['tableId']]]);
+
+              $db_timestamp = date_create_from_format('Y-m-d H:i:s', $row[$GLOBALS['TABLES'][$tableName]['timestamp']]);
+              $client_timestamp = date_create_from_format('Y-m-d H:i:s', $value1[$GLOBALS['TABLES'][$tableName]['timestamp']]);
+
+              if ($db_timestamp <= $client_timestamp) {
+                $result = getDB() -> delete($GLOBALS['TABLES'][$tableName]['tableName'],
+                                            [$GLOBALS['TABLES'][$tableName]['tableId'] =>  $value1[$GLOBALS['TABLES'][$tableName]['tableId']]]);
+              } else {
+                // the operation could not be `completed`
+                array_push($mergeList, $backup);
+              }
+            }
+          } else {
+            // no timestamp constraint --- proceeding with the deletion
+            foreach ($value as $key1 => $value1) {
+              $result = getDB() -> delete($GLOBALS['TABLES'][$tableName]['tableName'],
+                                          [$GLOBALS['TABLES'][$tableName]['tableId'] =>  $value1[$GLOBALS['TABLES'][$tableName]['tableId']]]);
+            }
+          }
+        break;
+
+
+
+        case 'SAVE':
+          // values that are not saved will be discarded
+          // BUT there's a little chance of this happening
+          // if the user is offline for like infinity time --- shit will be lost
+          // naa, am just kidding, there will be a merge list will be returned
+          // back to the sync requester
+          foreach ($value as $key1 => $value1) {
+            $backup = $value1;
+
+            unset($value1['operation']);
+            unset($value1[$GLOBALS['TABLES'][$tableName]['tableId']]);
+            $id = getDB() -> insert($GLOBALS['TABLES'][$tableName]['tableName'], $value1);
+            settype($id, 'integer');
+
+            if ($id === 0) {
+              array_push($mergeList, $backup);
+            }
+          }
+        break;
+
+
+
+        case 'UPDATE':
+          foreach ($value as $key1 => $value1) {
+            $backup = $value1;
+
+            if (isset($GLOBALS['TABLES'][$tableName]['timestamp'])) {
+              $row = getDB() -> get($GLOBALS['TABLES'][$tableName]['tableName'],
+                                    $GLOBALS['TABLES'][$tableName]['tableSelect'],
+                                    [$GLOBALS['TABLES'][$tableName]['tableId'] => $backup[$GLOBALS['TABLES'][$tableName]['tableId']]]);
+
+              if ($row) {
+                // row exits in the server, time to check on the timestamp son
+                $db_timestamp = date_create_from_format('Y-m-d H:i:s', $row[$GLOBALS['TABLES'][$tableName]['timestamp']]);
+                $client_timestamp = date_create_from_format('Y-m-d H:i:s', $value1[$GLOBALS['TABLES'][$tableName]['timestamp']]);
+
+                if ($db_timestamp > $client_timestamp) {
+                  // server has the latest version, ABANDON ship!
+                  array_push($mergeList[$key], $backup);
+                } else {
+                  // ERYthing looks good, continue with the merge
+                  unset($value1['operation']);
+                  unset($value1[$GLOBALS['TABLES'][$tableName]['tableId']]);
+                  $result = getDB() -> update($GLOBALS['TABLES'][$tableName]['tableName'],
+                                              $value1,
+                                              [$GLOBALS['TABLES'][$tableName]['tableId'] => $backup[$GLOBALS['TABLES'][$tableName]['tableId']]]);
+
+                  if ($result === 1 || $result === 0) {
+                    // update was a 'success' #BOOM!
+                  } else {
+                    // `conflict` detected on update
+                    // pushing to mergeList
+                    array_push($mergeList, $backup);
+                  }
+                }
+              } else {
+                // the update request came from an offline resource
+                // merging with server db, assuming there's no collision
+                unset($value1['operation']);
+                unset($value1[$GLOBALS['TABLES'][$tableName]['tableId']]);
+                $result = getDB() -> update($GLOBALS['TABLES'][$tableName]['tableName'],
+                                            $value1,
+                                            [$GLOBALS['TABLES'][$tableName]['tableId'] => $backup[$GLOBALS['TABLES'][$tableName]['tableId']]]);
+
+                if ($result === 1 || $result === 0) {
+                  // update was a 'success' #BOOM!
+                } else {
+                  // conflict detected on update
+                  // pushing to mergeList
+                  array_push($mergeList, $backup);
+                }
+              }
+            } else {
+              unset($value1['operation']);
+              unset($value1[$GLOBALS['TABLES'][$tableName]['tableId']]);
+              $result = getDB() -> update($GLOBALS['TABLES'][$tableName]['tableName'],
+                                          $value1,
+                                          [$GLOBALS['TABLES'][$tableName]['tableId'] => $backup[$GLOBALS['TABLES'][$tableName]['tableId']]]);
+
+              if ($result === 1 || $result === 0) {
+                // update was a 'success' #BOOM!
+              } else {
+                // conflict detected on update
+                // pushing to mergeList
+                array_push($mergeList, $backup);
+              }
+            }
+          }
+        break;
+      }
+    }
+
+
+
+    // drum roll...
+    $list = getDB() -> select($GLOBALS['TABLES'][$tableName]['tableName'],
+                                $GLOBALS['TABLES'][$tableName]['tableSelect']);
+
+    Flight::json(['LIST' => $list, 'MERGE' => $mergeList], 202);
+  }
 ?>
